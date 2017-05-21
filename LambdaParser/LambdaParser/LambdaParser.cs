a7658e33fe735abc43a97bc077f9349f7067b1ae
egorikas <egorgrishechko@gmail.com>
2017-05-21 23:20:37 +0300

    Initial commit
    Adding first version of extractor

---
+++ LambdaParser/LambdaParser/LambdaParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace LambdaParser
+{
+    public static class LambdaVariableExtractor
+    {
+        public static TValue ExtractValue<TValue>(this Delegate lambda, string variableName)
+            where TValue : class
+        {
+            if (lambda == null)
+                throw new NullReferenceException("Empty lambda");
+
+            var field = lambda.Method.DeclaringType?.GetFields
+                (
+                    BindingFlags.NonPublic |
+                    BindingFlags.Instance |
+                    BindingFlags.Public |
+                    BindingFlags.Static
+                )
+                .Single(x => x.Name == name);
+
+            if (field == null)
+                throw new NullReferenceException("There isn't a variable with this name");
+
+
+            return field.GetValue(lambda.Target) as TValue;
+        }
+    }
+}
