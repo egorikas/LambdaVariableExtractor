c711c6c0c2168609a5d02d8b95a84e1478110543
egorikas <egorgrishechko@gmail.com>
2017-06-20 23:00:21 +0300

    Change names and add unit-tests
    Change names and adding unit-tests

---
+++ LambdaVariableExtractor/LambdaVariableExtractorTest/ExtractValueTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LambdaVariableExtractor;
+using NUnit.Framework;
+
+namespace LambdaVariableExtractorTest
+{
+    [TestFixture]
+    public class ExtractValueTest
+    {
+        [Test]
+        public void ExtractValue_LambdaWithVariable_Succes()
+        {
+            //Arrange
+            var closure = "Outside";
+            Action actionWithClosure = () =>
+            {
+                closure = "Inside";
+            };
+
+            //Act
+            var result = actionWithClosure.ExtractValue<string>("closure");
+
+            //Assert
+            Assert.That(result, Is.EqualTo(closure));
+        }
+    }
+}
